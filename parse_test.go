61fad29928895f2a78347355745eb29ba0afcdd6
Russ Bradberry <rbradberry@gmail.com>
2014-12-05 10:53:12 -0500

    check for parse errors

--- parse_test.go
+++ parse_test.go
@@ -32,12 +32,18 @@ func TestParseDateString(t *testing.T) {
 
 func BenchmarkParseDateString(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		ParseDateString("2014-12-05 09:51:20.939152 -0500")
+		_, err := ParseDateString("2014-12-05 09:51:20.939152 -0500")
+		if err != nil {
+			b.Fatal(err)
+		}
 	}
 }
 
 func BenchmarkGoTimeParse(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		time.Parse("2006-01-02 15:04:05.999999 -0700", "2014-12-05 09:51:20.939152 -0500")
+		_, err := time.Parse("2006-01-02 15:04:05.999999 -0700", "2014-12-05 09:51:20.939152 -0500")
+		if err != nil {
+			b.Fatal(err)
+		}
 	}
 }
