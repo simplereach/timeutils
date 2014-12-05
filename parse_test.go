141f0f4efa61823cb03ea2ba34b807fea9f038b3
Russ Bradberry <rbradberry@gmail.com>
2014-12-05 10:48:49 -0500

    wrong order for parse

--- parse_test.go
+++ parse_test.go
@@ -39,6 +39,6 @@ func BenchmarkParseDateString(b *testing.B) {
 
 func BenchmarkGoTimeParse(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		time.Parse("2014-12-05 09:51:20.939152 -0500", "2006-01-02 15:04:05.999999 -0700")
+		time.Parse("2006-01-02 15:04:05.999999 -0700", "2014-12-05 09:51:20.939152 -0500")
 	}
 }
